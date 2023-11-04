bbe8cdbb8a7c659b66d1c6c4e27f42be34c255e5
Miturralde <57236319+mikelitu369@users.noreply.github.com>
2023-11-04 14:15:28 +0100

    Cartas Sigilo 1

---
+++ Assets/Scripts/Cards/Effects/Auras/AcelerarEffect.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+namespace MKCL.Cards
+{
+    [CreateAssetMenu(fileName = "Acelerar Effect", menuName = "Combat Effects/Auras/Acelerar")]
+    public class AcelerarEffect : CombatEffect
+    {
+        public override void Effect(int value)
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Cards/Effects/Generics/DrawEffect.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+namespace MKCL.Cards
+{
+    [CreateAssetMenu(fileName = "Draw Effect", menuName = "Combat Effects/Draw")]
+    public class DrawEffect : CombatEffect
+    {
+        public override void Effect(int value)
+        {
+
+        }
+    }
+}
