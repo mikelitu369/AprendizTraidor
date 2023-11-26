dab33c6191b641e09cd080bfdfa25b5409746ec7
Clantanab <73425709+Clantanab@users.noreply.github.com>
2023-11-26 19:31:03 +0100

    cardSet functionality

--- Assets/Scripts/Cards/CardSet.cs
+++ Assets/Scripts/Cards/CardSet.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 
 namespace MKCL.Cards
 {
@@ -13,6 +13,64 @@ namespace MKCL.Cards
         public void Shuffle()
         {
             cards.Shuffle();
+        }
+
+
+
+        /// <summary>
+        /// Takes the number of specified top cards/last cards and return a list removing them
+        /// from the deck in the process
+        /// </summary>
+        /// <param name="numberOfCards"></param>
+        /// <returns></returns>
+        public List<CardData> TakeFirstCards(int numberOfCards)
+        {
+            List<CardData> returnValue = new List<CardData>();
+            returnValue = cards.TakeLast(numberOfCards).ToList();
+
+            for (int i = 0; i < numberOfCards; ++i)
+            {
+                cards.RemoveAt(cards.Count - 1);
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Takes a number of the specified card returning a list and removing them
+        /// from the deck in the process
+        /// </summary>
+        /// <param name="nummberOfCards"> Number of times we want to find that card</param>
+        /// <param name="card"> Card to search for</param>
+        /// <returns></returns>
+        public List<CardData> TakeNumberSpecificCards(int nummberOfCards, CardData card)
+        {
+            List<CardData> returnValue = new List<CardData>();
+
+            for (int i = 0; i < nummberOfCards; ++i)
+            {
+                int index = cards.FindIndex((s) => s == card);
+                if(index == -1)
+                {
+                    Debug.LogWarning("Tried to seearch for more than the current ammount of" +
+                        " copys of the card in the deck");
+
+                    break;
+                }
+                returnValue.Add(cards[index]);
+                cards.RemoveAt(index);
+            }
+
+            return returnValue;
+        }
+
+        public List<CardData> TakeAllCopysOfSpecificCard(CardData card)
+        {
+            List<CardData> returnValue = new List<CardData>();
+
+            returnValue = cards.FindAll((s) => s == card).ToList();
+
+            return returnValue;
         }
     }
 }
